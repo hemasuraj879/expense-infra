bca742a7a74a807b2100f0ca6ed229023233fa78
hemasuraj879 <your_email@example.com>
2024-12-11 14:05:19 +0530

    files updating

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -1,20 +1,22 @@
+
+
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.66.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 
   backend "s3" {
-    bucket = "81s-remote-state-dev"
-    key    = "expense-vpc"
-    region = "us-east-1"
-    dynamodb_table = "81s-locking-dev"
+    bucket         = "roboshop-aws-infra-dev"
+    key            = "vpc-dev"
+    region         = "us-east-1"
+    dynamodb_table = "aws_infra"
   }
 }
 
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
 }
+
